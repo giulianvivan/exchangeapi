6be6dc189875c679292a7f8645e645db72641efa
Giulian Gonçalves Vivan <giulian@siliconant.com>
2023-07-25 21:20:58 -0300

    tools/send_post.sh: update to test on render

--- tools/send_post.sh
+++ tools/send_post.sh
@@ -2,4 +2,4 @@
 
 BASEDIR=$(dirname "$0")
 
-curl -X POST -H "Content-Type: application/json" -d @${BASEDIR}/example.json http://localhost:5000/convert
+curl -X POST -H "Content-Type: application/json" -d @${BASEDIR}/example.json https://exchangeapi-f2v1.onrender.com/convert
